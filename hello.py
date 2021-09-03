1c6b5de483f008012d4d9f10b21768c368a76873
Bavneet Singh <17bsingh@gmail.com>
2021-09-03 17:48:50 +0100

    Made Name Page

--- hello.py
+++ hello.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+from flask.helpers import flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Create a Flask Instance
 app=Flask(__name__)
-
+app.config['SECRET_KEY']="Royalarsenal121-"
 #Create a route decorate (URL)
+
+class NamerForm(FlaskForm):
+    name=StringField("Whats's Your Name?",validators=[DataRequired()])
+    submit=SubmitField("Submit")
+
 @app.route('/')
 
 #def index():
@@ -28,3 +37,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"),500
+
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name=None
+    form=NamerForm()
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+        flash("Form Submitted Successfully")
+
+
+    return render_template("name.html",
+    name=name,
+    form=form)
